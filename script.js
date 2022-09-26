278298ea3750e62b6178dae0a9cd09d9ce1e7a8c
davjh <dillonsdad91@gmail.com>
2022-09-26 18:33:46 -0400

    added local storage to js file and save button

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-var currDate = moment().format("dddd, MMMM Do");
+$(document).ready(function(){
+    var currDate = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currDate);
+
+
+$(".saveBtn").on("click",function(){
+    var value = $(this).siblings(".description").val();
+    var key = $(this).parent().attr("id");
+    localStorage.setItem(key,value)
+})
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+
+
+
+
+
+
+
+
+})
+
 
